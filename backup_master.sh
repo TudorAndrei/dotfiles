f48f6e27875b9b6e9264aa6dc0a2083a95188079
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2020-11-15 10:55:22 +0100

    updated dotfiles

--- backup_master.sh
+++ backup_master.sh
@@ -6,5 +6,6 @@ cp ~/.vimrc .
 cp ~/.config/i3/config ./i3/.
 cp ~/.config/i3blocks/i3blocks.conf ./i3/.
 rsync -ar --exclude=~/.config/nvim/autoload ~/.config/nvim .
+rm -rf ./nvim/autoload
 cp -ar ~/.config/compton/ .
 echo "Done"
