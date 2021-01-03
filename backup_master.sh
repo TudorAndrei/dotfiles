8148ea4bd43ac60d428a48751931737f066fc1f9
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2021-01-03 23:39:46 +0200

    Updates

--- backup_master.sh
+++ backup_master.sh
@@ -3,10 +3,13 @@
 
 cp ~/.zshrc ./zsh/
 cp ~/.vimrc ./vim/
+cp ~/.config/nvim ./nvim/init.vim
 
-rsync -ar --exclude=~/.config/nvim/autoload ~/.config/nvim .
 rsync -ar ~/.config/alacritty .
 rsync -ar ~/.config/i3 .
+rsync -ar ~/.config/i3blocks ./i3
+rsync -ar /usr/share/i3blocks ./i3-scripts/
+rsync -ar ~/.config/redshift .
 
 rm -rf ./nvim/autoload
 cp -ar ~/.config/compton/ ..
