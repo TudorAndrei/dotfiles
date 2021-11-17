10068eaf44fa884b5cdf1c2faf1425a9e6b8afd5
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2021-11-17 13:24:20 +0100

    git update

--- backup_master.sh
+++ backup_master.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
 
-cp ~/.zshrc ./zsh/
-cp ~/.vimrc ./vim/
 cp ~/.config/nvim/init.vim ./nvim/init.vim
-cp -ar ~/.config/redshift .
-cp ~/.config/starship.toml .
-
-rsync -ar ~/.config/i3 .
-rsync -ar ~/.config/i3blocks ./i3
-rsync -ar /usr/share/i3blocks ./i3-scripts/
 rsync -ar ~/.config/redshift .
 
 echo "Done"
