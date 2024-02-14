84c5c3745a5c3ae187437b2d5987dd0d3348716d
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-02-14 11:01:34 +0200

    updated dotfiles

--- create_links.sh
+++ create_links.sh
@@ -1,12 +1,12 @@
 DOT_DIR="~/dotfiles"
 CONFING_DIR="~/.config"
 
-ln -sf $DOT_DIR/zsh/.zshrc ~/.zshrc
-ln -sf $DOT_DIR/.tmux.conf ~/.tmux.conf
-ln -sf $DOT_DIR/starship.toml $CONFING_DIR/starship.toml
-ln -sf $DOT_DIR/alacritty/alacritty.yml $CONFING_DIR/alacritty/alacritty.yml
-ln -sf $DOT_DIR/.zimrc $CONFING_DIR/.zimrc
-
-ln -sf $DOT_DIR/i3/config $CONFING_DIR/i3/config
-ln -sf $DOT_DIR/redshift/redshift.conf $CONFING_DIR/redshift/redshift.conf
-ln -sf $DOT_DIR/.screenlayout ~/.screenlayout
+# ln -sf $DOT_DIR/zsh/.zshrc ~/.zshrc
+# ln -sf $DOT_DIR/.tmux.conf ~/.tmux.conf
+# ln -sf $DOT_DIR/starship.toml $CONFING_DIR/starship.toml
+ln -sf $DOT_DIR/alacritty/alacritty.toml $CONFING_DIR/alacritty/
+# ln -sf $DOT_DIR/.zimrc $CONFING_DIR/.zimrc
+#
+# ln -sf $DOT_DIR/i3/config $CONFING_DIR/i3/config
+# ln -sf $DOT_DIR/redshift/redshift.conf $CONFING_DIR/redshift/redshift.conf
+# ln -sf $DOT_DIR/.screenlayout ~/.screenlayout
